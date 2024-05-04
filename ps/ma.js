83cbf00b1e333364efa9c4a4cac2ec8f0a3edb50
gourub <149968940+gourub@users.noreply.github.com>
2024-05-04 16:27:45 -0600

    Add files via upload

---
+++ ps/ma.js
@@ -0,0 +1,12 @@
+// Menu.
+			$('#menu')
+				.append('<a href="#menu" class="close"></a>')
+				.appendTo($body)
+				.panel({
+					delay: 500,
+					hideOnClick: true,
+					hideOnSwipe: true,
+					resetScroll: true,
+					resetForms: true,
+					side: 'right'
+				});
